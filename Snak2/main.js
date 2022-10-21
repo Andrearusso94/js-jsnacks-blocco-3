5204307bbfd3de5e449f76901aa6159bf5ae5922
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-21 12:16:07 +0200

    end ex

--- Snak2/main.js
+++ Snak2/main.js
@@ -1,4 +1,18 @@
 /*A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
 Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]*/
 
-const words = ['pippo', 'PLUTO', 'Paperino']
+const words = ['pippo', 'PLUTO', 'Paperino', 'pAPERINA', 'GASGAS']
+
+console.log(words);
+
+//creo nuovo array usando .map
+const wordsMaiusc = words.map(word => {
+
+    let singleWord = word.slice(1).toLowerCase();
+    let firstLetter = word[0].toUpperCase();
+    
+    word = firstLetter + singleWord;
+    return word;
+});
+
+console.log(wordsMaiusc);
