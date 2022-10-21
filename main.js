399427c472ac769d1d6bf5d8bf65088eedf3d6aa
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-21 11:09:58 +0200

    initial commit snak and array

---
+++ main.js
@@ -0,0 +1,63 @@
+/*Snack 1:
+Crea un array composto da 10 automobili.
+Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+Infine stampa separatamente i 3 array.*/
+
+//creo array di 10 automobili
+
+const cars = [
+    {
+        marca: 'opel',
+        modello: 'corsa',
+        alimentazione: 'benzina',
+    },
+    {
+        marca: 'opel',
+        modello: 'mokka',
+        alimentazione: 'diesel',
+    },
+    {
+        marca: 'opel',
+        modello: 'karl',
+        alimentazione: 'ibrido',
+    },
+    {
+        marca: 'ford',
+        modello: 'fiesta',
+        alimentazione: 'ibrido',
+    },
+    {
+        marca: 'ford',
+        modello: 'puma',
+        alimentazione: 'benzina',
+    },
+    {
+        marca: 'ford',
+        modello: 'mustang',
+        alimentazione: 'benzina',
+    },
+    {
+        marca: 'opel',
+        modello: 'corsa',
+        alimentazione: 'benzina',
+    },
+    {
+        marca: 'opel',
+        modello: 'mokka',
+        alimentazione: 'diesel',
+    },
+    {
+        marca: 'opel',
+        modello: 'karl',
+        alimentazione: 'ibrido',
+    },
+    {
+        marca: 'ford',
+        modello: 'fiesta',
+        alimentazione: 'ibrido',
+    },
+   
+]
+
+console.log(cars)
